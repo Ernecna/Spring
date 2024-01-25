4abf7ed3727e78b2f89df856157ad15f9208cba9
Erencan Yakut <97342233+Ernecna@users.noreply.github.com>
2024-01-26 00:04:04 +0300

    Spring-Boot-Starting-Overview
    Some basics
    Some Starting info and fundamental for Spring Framework

---
+++ 01-spring-boot-overiview/05-properties-demo/src/main/java/com/example/myapp/rest/FunRestController.java
@@ -0,0 +1,37 @@
+package com.example.myapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // inject properties for : coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private  String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public  String infoTeam(){
+        return "teamh Name "+teamName+" coachname "+coachName;
+    }
+
+
+
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHelloWorld(){
+        return "Hello World";
+    }
+
+    //expose a new endpoint for workout
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "DEVTOLL1";
+    }
+}
